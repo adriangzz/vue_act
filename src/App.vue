83f5d6b2cde6099a726bd81f9f9c75758100878b
Adrian Gzz <adriangonzalezmontemayor@gmail.com>
2022-03-30 10:56:33 -0600

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import Title from "./components/Title.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <Title title="Pets App" />
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  margin: 2rem;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ title }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
